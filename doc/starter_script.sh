5836d2c731bd74e7ff544110a44b66fb2814b8c1
bsteve456 <stevealan1@protonmail.com>
2023-03-24 15:55:21 +0100

    add some install openssl and openssh-server

--- doc/starter_script.sh
+++ doc/starter_script.sh
@@ -7,7 +7,9 @@ sudo apt-get install \
 	vim \
     curl \
     gnupg \
-	terminator
+	terminator \
+	openssl \
+	openssh-server
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \

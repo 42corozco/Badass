b72d89d71cbc15e01f4ddb66e0f2f659affe516c
42corozco <42corozco@gmail.com>
2023-03-29 02:55:51 +0200

    update 1

--- P1/setup_part1.sh
+++ P1/setup_part1.sh
@@ -1,1 +1,8 @@
-docker build -t $USER -f Dockerfile.host .
+username=$USER
+routername=${username}_router
+hostname=${username}_host
+
+docker build -t $hostname -f Dockerfile.host .
+docker build -t $routername -f Dockerfile.router .
+
+
